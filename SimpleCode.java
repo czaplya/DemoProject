e080f71ac601f674926e464a199e26fd073f1572
artemnesterov <czaplya@gmail.com>
2023-11-15 19:39:25 -0500

    SimpleCode class added

---
+++ SimpleCode.java
@@ -0,0 +1,13 @@
+public class SimpleCode {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 6;
+        System.out.println(sum(a,b));
+
+    }
+
+    public static int sum (int a, int b){
+        return a+b;
+    }
+}
